b9126893ba884dea662d16f8123b9621a3c540ac
Bongo Russom <jrussom@vrbo.com>
2020-10-22 23:04:58 -0500

    1.36

--- problems/1.36.scm
+++ problems/1.36.scm
@@ -6,19 +6,14 @@
       (< (abs (- v1 v2)) tolerance))
     (define (try guess)
       (let ((next (f guess)))
+        (display guess)
+        (newline)
         (if (close-enough? guess next)
             next
             (try next))))
     (try first-guess))
   
 
-  (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
+  (fixed-point (lambda (x) (/ (log 1000) (log x))) 2.0))
 (test-proc)
 
-
-; x -> 1 + 1/x
-; (1 + sqrt(5))/2 -> 1 + 2/(1 + sqrt(5))
-; = (1 + sqrt(5) + 2)/(1 + sqrt(5)) = (3 + sqrt(5))/(1 + sqrt(5))
-; = (3 + sqrt(5))(1 - sqrt(5))/(-4) = (3 - 2sqrt(5) - 5)/(-4)
-; = (2sqrt(5) + 2)/4 = (1 + sqrt(5)/2 
-; QED
