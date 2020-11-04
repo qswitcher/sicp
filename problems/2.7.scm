a2178ba48d10e83668d071ecf56f4b627094637d
Jeffrey Russom <jrussom@indeed.com>
2020-11-04 00:39:24 -0600

    2.7

---
+++ problems/2.7.scm
@@ -0,0 +1,8 @@
+#lang racket
+(define (run-test) 
+  (define (make-interval a b) (cons a b))
+  (define (upper-bound z) (max (car z) (cdr z)))
+  (define (lower-bound z) (min (car z) (cdr z)))
+
+  )
+(run-test)
