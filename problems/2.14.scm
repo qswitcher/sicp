8d2e728dc8f32548eb7640fede8196481ea81cf8
Jeffrey Russom <jrussom@indeed.com>
2020-11-06 23:29:32 -0600

    2.14

---
+++ problems/2.14.scm
@@ -0,0 +1,38 @@
+#lang racket
+(define (run-test) 
+  (define (make-interval a b) (cons a b))
+  (define (upper-bound z) (max (car z) (cdr z)))
+  (define (lower-bound z) (min (car z) (cdr z)))
+
+  (define (add-interval x y)
+    (make-interval (+ (lower-bound x) (lower-bound y))
+                   (+ (upper-bound x) (upper-bound y))))
+  (define (mul-interval x y)
+    (let ((p1 (* (lower-bound x) (lower-bound y)))
+          (p2 (* (lower-bound x) (upper-bound y)))
+          (p3 (* (upper-bound x) (lower-bound y)))
+          (p4 (* (upper-bound x) (upper-bound y))))
+      (make-interval (min p1 p2 p3 p4)
+                     (max p1 p2 p3 p4))))
+  (define (div-interval x y)
+    (mul-interval x 
+                  (make-interval (/ 1.0 (upper-bound y))
+                                 (/ 1.0 (lower-bound y)))))
+  
+  (define (par1 r1 r2)
+    (div-interval (mul-interval r1 r2)
+                  (add-interval r1 r2)))
+  (define (par2 r1 r2)
+    (let ((one (make-interval 1 1))) 
+      (div-interval one
+                    (add-interval (div-interval one r1)
+                                  (div-interval one r2)))))
+
+  (define z (make-interval 2 2.001))
+  (define x (make-interval 3 3.001))
+  (define y (make-interval 4 4.001))
+  (display (par1 z z))
+  (newline)
+  (display (par2 z z))
+  )
+(run-test)
