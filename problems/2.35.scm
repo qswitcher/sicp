b3f804ef397d7234106fe73f6fcfd747db172e22
Jeffrey Russom <jrussom@indeed.com>
2020-11-14 23:28:55 -0600

    2.34-2.35

---
+++ problems/2.35.scm
@@ -0,0 +1,16 @@
+#lang racket
+(define (run-test)
+  (define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence)
+            (accumulate op initial (cdr sequence)))))
+  
+  (define (count-leaves t)
+    (accumulate + 0 (map (lambda (x) (if (pair? x) (count-leaves x) 1)) t)))
+
+  (define x (cons (list 1 2) (list 3 4)))
+
+  (count-leaves x)
+  )
+(run-test)
